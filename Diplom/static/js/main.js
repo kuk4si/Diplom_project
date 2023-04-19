b2c77e321822ca984284c02e5a175606a66bbe9c
Андрей Андрей <KUKASI222@outlook.com>
2023-04-19 12:47:32 +0200

    Style Update

---
+++ Diplom/static/js/main.js
@@ -0,0 +1,6 @@
+//fetch('http://ip-api.com/json/')
+//.then(d => d.json())
+
+//.then(d => document.querySelector('#query').innerHTML = d.query);
+//.then(d => document.querySelector('#regionName').innerHTML = d.regionName);
+//.then(d => document.querySelector('#country').innerHTML = d.country);
